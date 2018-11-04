afa096f6f814367746dc3801015d8a42d4eb2bfe
artonge <artonge@chmn.me>
2018-11-04 12:14:21 +0100

    Update setup.py Fix #1

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+import setuptools
 
-setup(name='gym_sudoku',
-	version='1.0.2',
+setuptools.setup(name='gym_sudoku',
+	version='1.0.3',
 	install_requires=['gym'],
 	description='Sudoku environment for OpenAI gym',
 	license=open('LICENCE', "r").read(),
 	long_description=open('README.md', "r").read(),
 	keywords='sudoku,openai,gym,environment',
 	author='artonge',
+	packages=setuptools.find_packages(),
 	author_email='artonge@chmn.me',
 	url='https://github.com/artonge/sudokuEnv'
 )
